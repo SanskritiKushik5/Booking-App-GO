44ea263c86011f568866a2edfc26e25741caa59e
SanskritiKushik5 <sanskritikushik23@gmail.com>
2022-05-11 18:47:38 +0530

    details

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your tickets here to attend")
 }
