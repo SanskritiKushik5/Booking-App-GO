5aef71c4f79f6222b7c2e940b8090f95d3fba3f0
SanskritiKushik5 <sanskritikushik23@gmail.com>
2022-05-11 23:17:12 +0530

    datatypes

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint= 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+	// ask user for number of tickets
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
+
+// Two basic data types - Strings and Integers
+// Others - Booleans, float, Maps, Arrays
+
+// %T is for Type of Variables
