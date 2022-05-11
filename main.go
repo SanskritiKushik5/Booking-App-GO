c4f89b83a54cae8be24ce7c5d2a167bb6e86f7db
SanskritiKushik5 <sanskritikushik23@gmail.com>
2022-05-12 00:52:06 +0530

    scan

--- main.go
+++ main.go
@@ -11,13 +11,24 @@ func main() {
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	// ask user for their name
-	// ask user for number of tickets
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter no. of tickets: ")
+	fmt.Scan(&userTickets)
+	
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, email, userTickets)
 }
 
 // Two basic data types - Strings and Integers
